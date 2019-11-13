8e36bc49368c8d2913d6611a9c44a817f87d2c12
Lindsey Heagy <lindseyheagy@gmail.com>
2019-11-13 10:47:47 -0800

    Update test_notebooks.py

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,16 +4,10 @@ import testipynb
 import unittest
 
 NBDIR = os.path.sep.join(
-    os.path.abspath(__file__).split(os.path.sep)[:-2] + ['notebooks']
+    os.path.abspath(__file__).split(os.path.sep)[:-2]
 )
 
-IGNORE = ["3_DC_Kaufman_finite_well", "5_FDEM_following_Augustin_Fig3"]
-
-n_ignore = 3  # so we don't run over-time on travis, randomly ignore 3 notebooks
 Test = testipynb.TestNotebooks(directory=NBDIR, timeout=2800)
-ignore_inds = np.random.choice(len(Test._nbnames) - len(IGNORE), n_ignore)
-test_nbnames = [t for t in Test._nbnames if t not in IGNORE]
-Test.ignore = IGNORE + [test_nbnames[i] for i in ignore_inds]
 TestNotebooks = Test.get_tests()
 
 if __name__ == "__main__":
